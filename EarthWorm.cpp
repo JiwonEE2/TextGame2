ac2880c5763aa1d27a38c2c72d9f5637b2a1de58
Nori <jiwonchichi1@gmail.com>
2024-10-02 17:46:16 +0900

    241002 17:46 UnitManager을 만들어서 정리좀 해보자 ㅎ

--- EarthWorm.cpp
+++ EarthWorm.cpp
@@ -14,7 +14,7 @@ EarthWorm::EarthWorm()
 
 void EarthWorm::Print() const
 {
-	cout << name << "가 x : " << x << ", y : " << y << "에 생성되었습니다.\n";
+	//cout << name << "가 x : " << x << ", y : " << y << "에 생성되었습니다.\n";
 }
 
 int EarthWorm::GetExperience()
